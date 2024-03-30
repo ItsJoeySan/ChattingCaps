ef2182f1b535a26aab942e24e0b72f7e1f8e8f1c
ItsJoey <ItsJoeySan@gmail.com>
2024-03-30 22:19:44 +0530

    created middleware for redirection, EmptyState, and Initialized Sidebar and conversation page,

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,18 +2,27 @@
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 type Variant = "LOGIN" | "REGISTER";
 const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      router.push("/users");
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
@@ -42,6 +51,7 @@ const AuthForm = () => {
       //Axios Register
       axios
         .post("/api/register", data)
+        .then(() => signIn("credentials", data))
         .catch((error) => toast.error("Something went wrong!"))
         .finally(() => setIsLoading(false));
     }
@@ -59,6 +69,7 @@ const AuthForm = () => {
 
           if (callback?.ok && !callback?.error) {
             toast.success("Logged in!");
+            router.push("/users");
           }
         })
         .finally(() => setIsLoading(false));
@@ -77,6 +88,7 @@ const AuthForm = () => {
 
         if (callback?.ok && !callback?.error) {
           toast.success("Logged in!");
+          router.push("/users");
         }
       })
       .finally(() => setIsLoading(false));
