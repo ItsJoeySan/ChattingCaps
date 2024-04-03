efd10630f03aa2be3d5a91284efebcf362942391
ItsJoey <ItsJoeySan@gmail.com>
2024-04-03 20:52:25 +0530

    showing users list on users tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ToasterContext from "./context/ToasterContext";
 import AuthContext from "./context/AuthContext";
+import getUsers from "./actions/getUsers";
+import UserList from "./users/components/UserList";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,16 +13,18 @@ export const metadata: Metadata = {
   description: "A chatting app for every friend of mine -Joey ",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const users = await getUsers();
   return (
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
           <ToasterContext />
+          <UserList items={users} />
           {children}
         </AuthContext>
       </body>
